377fe56db193d5de09c6d364fd6bdbe127d065b2
Pooja31pc <pooja31pc@gmail.com>
2020-12-09 11:01:22 +0530

    initial commit for jenkins

---
+++ src/main/java/com/example/jenkins/jenkinsexample/JenkinsSpringBootExampleApplication.java
@@ -0,0 +1,22 @@
+package com.example.jenkins.jenkinsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsSpringBootExampleApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World Spring Boot!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins/jenkinsexample/JenkinsSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.jenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
